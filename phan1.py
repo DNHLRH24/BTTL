498ac6ff92cb02928b92ac7915f9b53785870b4e
Quocdank3 <115203375+Quocdank3@users.noreply.github.com>
2022-12-23 19:47:12 +0700

    Add files via upload

---
+++ phan1.py
@@ -0,0 +1,40 @@
+import random
+import os
+import pickle
+n = int(input('nhap n:'))
+a = int(input('nhap a:'))
+b = int(input('nhap b:'))
+def list_so_thuc(a,b):
+    x=[(b-a)*random.random() +a for i in range(n)]
+    return x
+def list_tang_dan(x):
+    x=sorted(list_so_thuc(a,b),reverse=False)
+    return x
+def list_giam_dan(x):
+    x=sorted(list_so_thuc(a,b),reverse=True)
+    return x
+c=int(input('nhap c:'))
+def tim_kiem_trong_list(c):
+    try:
+      print('vi tri cua c la ', x.index(c))
+    except :
+      print('khong tim thay')
+m=input('nhap m:')
+def luu_list_vao_tap_tin(x):
+    path='C:/nnlttqd/doan'
+    filename='listsothuc.txt'
+    try:
+       with open(os.path.join(path,filename),mode=m) as x :
+           pickle.dump(list_so_thuc(a,b),x)
+           print('ket thuc qua trinh luu')
+    except Exception as x:
+        print('xay ra loi trong qua trinh luu')
+def main():
+    x=list_so_thuc(a,b)
+    print(x)
+    print(list_tang_dan(x))
+    print(list_giam_dan(x))
+    tim_kiem_trong_list(c)
+    luu_list_vao_tap_tin(x)
+if __name__ == '__main__':
+    main()
