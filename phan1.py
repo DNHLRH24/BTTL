ddc497faf2eb4b5a292805f7238639747df37a5d
Quocdank3 <115203375+Quocdank3@users.noreply.github.com>
2022-12-23 14:41:32 +0700

    Add files via upload

--- phan1.py
+++ phan1.py
@@ -1,10 +1,11 @@
-﻿import random
+import random
 import os
 import pickle
+import random
+n = int(input('nhap n:'))
+a = int(input('nhap a:'))
+b = int(input('nhap b:'))
 def list_so_thuc(a,b):
-    n=int(float('các giá trị số thực n:'))
-    a=int(float('nhập a='))
-    b=int(lloat('nhập b='))
     x=[(b-a)*random.random() +a for i in range(n)]
     return x
 def list_tang_dan(x):
@@ -13,20 +14,18 @@ def list_tang_dan(x):
 def list_giam_dan(x):
     x=sorted(list_so_thuc(a,b),reverse=True)
     return x
+c=int(input('nhap c:'))
 def tim_kiem_trong_list(c):
     try:
       print('vi tri cua c la ', x.index(c))
     except :
       print('khong tim thay')
 def luu_list_vao_tap_tin(x):
-    path='C://Users//Administrator//source//repos//doan'
-    filename='luudo.txt'
-    try:
-      with open(os.path.join(path,filename),'wb') as x : 
+    path='C:/nnlttqd/doan'
+    filename='listsothuc.txt'
+    with open(os.path.join(path,filename),'wb') as x :
         pickle.dump(list_so_thuc(a,b),x)
         print('ket thuc qua trinh luu')
-    except :
-        print('không luu do lỗi tệp')
 def main():
     x=list_so_thuc(a,b)
     print(x)
